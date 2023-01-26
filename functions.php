46ea759bf477e68688bb7ef0fbe21e45c2ed273d
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2023-01-26 23:51:44 +0500

    Adding post images and thumbnails

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function shayboniyshoh_title_tag()
 {
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'shayboniyshoh_title_tag');
 // end of adding WordPress Dynamic title
